7e94e92591e66f35a620e55e626fd72472ecd242
Dominic Monn <dm@impac.ch>
2016-04-21 08:14:15 +0200

    added custom dependency

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'Django',
         'django-cms',
-        'django-forms-builder',
+        'django-forms-builder-08eins-custom',
     ],
     zip_safe=False
 )
