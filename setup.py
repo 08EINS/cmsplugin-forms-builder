5dc578364b546e0cbb98145c93856aa59581f492
Dominic Monn <dm@impac.ch>
2016-04-21 09:38:17 +0200

    added package credentials

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages, setup
 
 # setup the project
 setup(
-    name='cmsplugin-forms-builder',
-    version='1.0.1',
+    name='cmsplugin-forms-builder-08eins-custom',
+    version='2.0.0',
     description='django-cms plugin for cmsplugin-forms-builder',
     long_description=open('README.md').read(),
     author='Nimbis Services, Inc.',
     author_email='devops@nimbisservices.com',
-    url='https://github.com/nimbis/cmsplugin-forms-builder/',
+    url='https://github.com/08EINS/cmsplugin-forms-builder/',
     packages=find_packages(exclude=["tests", ]),
     license='BSD',
     classifiers=[
@@ -23,6 +23,9 @@ setup(
         'Framework :: Django',
     ],
     include_package_data=True,
+    find_links=[
+
+    ],
     install_requires=[
         'Django',
         'django-cms',
